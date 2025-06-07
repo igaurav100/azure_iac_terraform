e6fb34c288b37a91d0b8e9840524a7cd917d4699
Gaurav Sharma <igaurav1002@outlook.com>
2025-06-07 16:56:22 +0530

    Adding backend configs in providers file

--- terraform_three_tier_iac/provider.tf
+++ terraform_three_tier_iac/provider.tf
@@ -9,4 +9,12 @@ terraform {
 
 provider "azurerm" {
   # Configuration options
+  # terraform backend configuration
+  features {}
+  # Optional: Specify the Azure subscription ID if not set in the environment
+  # subscription_id = "your-subscription-id"
+  # Optional: Specify the Azure tenant ID if not set in the environment
+  # tenant_id = "your-tenant-id"
+  # Optional: Specify the Azure client ID if not set in the environment
+  # client_id = "your-client-id"
 }
